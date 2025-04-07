71e8ba9cdc00b5ec58221900db02ceb96a6b115c
Lorena Bucurú Rodríguez <nlbucurur@unal.edu.co>
2025-04-07 17:19:23 +0200

    ✨ Put all in one histogram.

--- plot_variables.C
+++ plot_variables.C
@@ -31,7 +31,6 @@ void plot_variables () {
   double P_mass = 0.938272;
   double N_mass = 0.9395654;
 
-  // TFile *file = TFile::Open("/mnt/d/Data/0pDVCS_inbending_FTPhotonsCorrected_test.root");
   TFile *file = TFile::Open("/home/lorena/Thesis/JLAB_EIC/0pDVCS_inbending_FTPhotonsCorrected_test.root");
 
   TTree *tree = (TTree*) file->Get("pDVCS_stripped");
@@ -47,20 +46,32 @@ void plot_variables () {
     {"_mm2_eNg", -2, 6, N_mass - 0.1, N_mass + 0.1}, // Expecting neutron mass
     {"_mm2_eNg_N", -2, 2, -0.2, 0.2}, // Expecting nothing
     {"_mm2_eg", -3, 6, P_mass - 0.1, P_mass + 0.1}, // Expecting missing proton
-    {"_mm2_eNX_N", -10, 10, -0.5, 0.5} // Expecting photon
+    {"_mm2_eNX_N", -10, 10, -0.5, 0.5}, // Expecting photon
+    {"_strip_El_chi2pid", -4, 4, 0, 1},
+    {"_strip_Ph_chi2pid", 0, 115, 0, 115},
+    {"_strip_Nuc_chi2pid", -1, 5, 0, 1}
   };
 
   gStyle->SetOptStat(1110);
+  TCanvas *canvas_all = new TCanvas("canvas 1D Histograms", "canvas 1D Histograms", 1800, 1600);
+  canvas_all->Divide(3, 4);
+
+  int canvas_entry = 1;
 
   for (const auto& branch_name : branch_names) {
 
-    TCanvas *canvas = new TCanvas(("canvas_" + branch_name.name).c_str(), branch_name.name.c_str(), 800, 600);
+    // TCanvas *canvas = new TCanvas(("canvas_" + branch_name.name).c_str(), branch_name.name.c_str(), 800, 600);
 
-    std::string histDef = ">>htemp(40," +
+    canvas_all->cd(canvas_entry);
+
+    std::string hist_name = "htemp_" + std::to_string(canvas_entry);
+    std::string hist_name_cut = "htemp_cut_" + std::to_string(canvas_entry);
+
+    std::string histDef = ">>" + hist_name + "(40," +
                           std::to_string(branch_name.min) + "," + std::to_string(branch_name.max) + ")";
 
 
-    std::string histDef_cut = ">>htemp_cut(40," +
+    std::string histDef_cut = ">>" + hist_name_cut + "(40," +
                           std::to_string(branch_name.min) + "," + std::to_string(branch_name.max) + ")";
 
 
@@ -72,10 +83,11 @@ void plot_variables () {
     
     tree->Draw((branch_name.name + histDef).c_str(), no_cut.c_str());
     tree->Draw((branch_name.name + histDef_cut).c_str(), cut.c_str(), "same");
-    TH1D *htemp = (TH1D*) gDirectory->Get("htemp");
-    TH1D *htemp_cut = (TH1D*) gDirectory->Get("htemp_cut");
     
-    htemp->SetName(("hist_" + branch_name.name).c_str());
+    TH1D *htemp = (TH1D*) gDirectory->Get(hist_name.c_str());
+    TH1D *htemp_cut = (TH1D*) gDirectory->Get(hist_name_cut.c_str());
+    
+    // htemp->SetName(("hist_" + branch_name.name).c_str());
     htemp->SetFillStyle(4050);
     htemp->SetFillColor(5);
     htemp->SetTitle(("DVCS " + branch_name.name).c_str());
@@ -89,57 +101,66 @@ void plot_variables () {
     htemp_cut->SetFillStyle(4050);
     htemp_cut->SetFillColor(38);
 
-    std::string output_file = "./plots/" + branch_name.name + "_plot.pdf";
-    canvas->SaveAs(output_file.c_str());
-    delete canvas;
+    htemp->Draw("HIST");
+    htemp_cut->Draw("HIST SAME");
+
+    canvas_entry++;
+
+    // std::string output_file = "./plots/" + branch_name.name + "_plot.pdf";
+    // canvas->SaveAs(output_file.c_str());
+    // delete canvas;
   }
+
+  std::string output_file = "./plots/Hist1D_plots.pdf";
+  canvas_all->SaveAs(output_file.c_str());
+  delete canvas_all;
 
   //****//
   // 2D //
   //****//
 
-  std::vector<branches_cuts_2D> branch_names_2D = {
-    {"Electron_P_vs_Phi", "_strip_El_Phi", "_strip_El_P", -180, 180, 0, 9},
-    {"Electron_Theta_vs_Phi", "_strip_El_Phi", "_strip_El_Theta", -180, 180, 0, 50},
+  // std::vector<branches_cuts_2D> branch_names_2D = {
+  //   {"Electron_P_vs_Phi", "_strip_El_Phi", "_strip_El_P", -180, 180, 0, 9},
+  //   {"Electron_Theta_vs_Phi", "_strip_El_Phi", "_strip_El_Theta", -180, 180, 0, 50},
     
-    {"Photon_P_vs_Phi", "_strip_Ph_Phi", "_strip_Ph_P", -180, 180, 0, 11},
-    {"Photon_Theta_vs_Phi", "_strip_Ph_Phi", "_strip_Ph_Theta", -180, 180, 0, 40},
+  //   {"Photon_P_vs_Phi", "_strip_Ph_Phi", "_strip_Ph_P", -180, 180, 0, 11},
+  //   {"Photon_Theta_vs_Phi", "_strip_Ph_Phi", "_strip_Ph_Theta", -180, 180, 0, 40},
     
-    {"Nucleon_P_vs_Phi", "_strip_Nuc_Phi", "_strip_Nuc_P", -180, 180, 0, 8},
-    {"Nucleon_Theta_vs_Phi", "_strip_Nuc_Phi", "_strip_Nuc_Theta", -180, 180, 0, 100}
-  };
+  //   {"Nucleon_P_vs_Phi", "_strip_Nuc_Phi", "_strip_Nuc_P", -180, 180, 0, 8},
+  //   {"Nucleon_Theta_vs_Phi", "_strip_Nuc_Phi", "_strip_Nuc_Theta", -180, 180, 0, 100}
+  // };
   
-  gStyle->SetOptStat(0);
+  // gStyle->SetOptStat(0);
   
-  for (const auto& branch_name_2D : branch_names_2D) {
-    TCanvas *canvas = new TCanvas(("canvas_" + branch_name_2D.name).c_str(), branch_name_2D.name.c_str(), 1300, 600);
+  // for (const auto& branch_name_2D : branch_names_2D) {
+  //   TCanvas *canvas = new TCanvas(("canvas_" + branch_name_2D.name).c_str(), branch_name_2D.name.c_str(), 1300, 600);
 
-    std::string histDef = ">>htemp(60," +
-                          std::to_string(branch_name_2D.x_min) + "," +
-                          std::to_string(branch_name_2D.x_max) + "," + "60," +
-                          std::to_string(branch_name_2D.y_min) + "," +
-                          std::to_string(branch_name_2D.y_max) + ")";
+  //   std::string histDef = ">>htemp(60," +
+  //                         std::to_string(branch_name_2D.x_min) + "," +
+  //                         std::to_string(branch_name_2D.x_max) + "," + "60," +
+  //                         std::to_string(branch_name_2D.y_min) + "," +
+  //                         std::to_string(branch_name_2D.y_max) + ")";
 
-    std::string cut = branch_name_2D.x_branch + " >= " + std::to_string(branch_name_2D.x_min) + " && " +
-                      branch_name_2D.x_branch + " <= " + std::to_string(branch_name_2D.x_max) + " && " +
-                      branch_name_2D.y_branch + " >= " + std::to_string(branch_name_2D.y_min) + " && " +
-                      branch_name_2D.y_branch + " <= " + std::to_string(branch_name_2D.y_max);
+  //   std::string cut = branch_name_2D.x_branch + " >= " + std::to_string(branch_name_2D.x_min) + " && " +
+  //                     branch_name_2D.x_branch + " <= " + std::to_string(branch_name_2D.x_max) + " && " +
+  //                     branch_name_2D.y_branch + " >= " + std::to_string(branch_name_2D.y_min) + " && " +
+  //                     branch_name_2D.y_branch + " <= " + std::to_string(branch_name_2D.y_max);
     
-    tree->Draw((branch_name_2D.y_branch + ":" + branch_name_2D.x_branch + histDef).c_str(), cut.c_str(), "COLZ");
-    TH2D *htemp = (TH2D*) gDirectory->Get("htemp");
+  //   tree->Draw((branch_name_2D.y_branch + ":" + branch_name_2D.x_branch + histDef).c_str(), cut.c_str(), "COLZ");
+  //   TH2D *htemp = (TH2D*) gDirectory->Get("htemp");
     
-    htemp->GetXaxis()->SetLimits(branch_name_2D.x_min, branch_name_2D.x_max);
-    htemp->GetYaxis()->SetLimits(branch_name_2D.y_min, branch_name_2D.y_max);
-    htemp->SetName(("hist_" + branch_name_2D.name).c_str());
-    htemp->SetTitle(("DVCS " + branch_name_2D.name).c_str());
-    htemp->GetXaxis()->SetTitle(branch_name_2D.x_branch.c_str());
-    htemp->GetYaxis()->SetTitle(branch_name_2D.y_branch.c_str());
+  //   htemp->GetXaxis()->SetLimits(branch_name_2D.x_min, branch_name_2D.x_max);
+  //   htemp->GetYaxis()->SetLimits(branch_name_2D.y_min, branch_name_2D.y_max);
+  //   htemp->SetName(("hist_" + branch_name_2D.name).c_str());
+  //   htemp->SetTitle(("DVCS " + branch_name_2D.name).c_str());
+  //   htemp->GetXaxis()->SetTitle(branch_name_2D.x_branch.c_str());
+  //   htemp->GetYaxis()->SetTitle(branch_name_2D.y_branch.c_str());
 
-    std::string output_file = "./plots/" + branch_name_2D.name + "_plot.pdf";
-    canvas->SaveAs(output_file.c_str());
-    delete canvas;
-    delete htemp;
-  };
+  //   std::string output_file = "./plots/" + branch_name_2D.name + "_plot.pdf";
+  //   canvas->SaveAs(output_file.c_str());
+  //   delete canvas;
+  //   delete htemp;
+  // };
 
   delete file;
  
