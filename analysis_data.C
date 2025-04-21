23f123c681366eae3fff2758b03f7ce584a37e46
Lorena Bucurú Rodríguez <nlbucurur@unal.edu.co>
2025-04-22 00:43:48 +0200

    🎨 Last run data division canvas.

--- analysis_data.C
+++ analysis_data.C
@@ -12,8 +12,6 @@
 #include <vector>
 #include <utility>
 
-// before refactoring
-
 bool should_set_logy(const TString &branch_name)
 {
   std::vector<TString> logy_branches = {
@@ -23,41 +21,8 @@ bool should_set_logy(const TString &branch_name)
   return std::find(logy_branches.begin(), logy_branches.end(), branch_name) != logy_branches.end();
 }
 
-bool should_move_stats(const TString &branch_name)
-{
-  std::vector<TString> move_stats = {
-      "_t_Nuc",
-      "_Phi_Nuc",
-      "_Phi_Ph",
-      "_strip_Ph_chi2pid"};
-
-  return std::find(move_stats.begin(), move_stats.end(), branch_name) != move_stats.end();
-}
-
-std::pair<TString, TString> auto_cut(const TString &var, TH1D *hist, const TString &cut_label)
-{
-  double mean = hist->GetMean();
-  double sigma = hist->GetStdDev();
-
-  double min_cut = mean - 3.0 * sigma;
-  double max_cut = mean + 3.0 * sigma;
-
-  TString cut = Form("%s >= %.5f && %s <= %.5f", var.Data(), min_cut, var.Data(), max_cut);
-
-  return {cut_label, cut};
-}
-
 std::vector<std::pair<TString, TCut>> generate_cuts(const std::map<TString, TH1D *> &hs_base)
 {
-  // std::vector<std::pair<TString, TString>> cuts_definitions = {
-  //     {"_theta_gamma_e", "_theta_gamma_e > 6"},
-  //     {"_chi2pid", ""},
-  //     {"_delta_t", ""},
-  //     {"_delta_phi", "abs(fmod(_delta_Phi, 180)) <= 1.5"},
-  //     {"_mm2_eNg_neutron_expected", ""},
-  //     {"_mm2_eNg_N_nothing_expected", ""},
-  //     {"_mm2_eNX_N_photon_expected", ""},
-  //     {"_mm2_eg_proton_expected", ""}};
 
   std::vector<std::pair<TString, TString>> cuts_definitions = {
       {"_theta_gamma_e", "_theta_gamma_e > 6"},
@@ -68,49 +33,6 @@ std::vector<std::pair<TString, TCut>> generate_cuts(const std::map<TString, TH1D
       {"_mm2_eNg_N_nothing_expected", "_mm2_eNg_N >= -0.19478 && _mm2_eNg_N <= 0.15635"},
       {"_mm2_eNX_N_photon_expected", "_mm2_eNX_N >= -3.95236 && _mm2_eNX_N <= 3.74568"},
       {"_mm2_eg_proton_expected", "_mm2_eg >= -0.12854 && _mm2_eg <= 2.21362"}};
-
-  // for (const auto& cut_def : cuts_definitions) {
-  //   std::cout << cut_def.first << ": " << cut_def.second << std::endl;
-  // }
-
-  // for (auto &[cut_label, cut] : cuts_definitions)
-  // {
-  //   if (cut == "")
-  //   {
-  //     if (cut_label == "_chi2pid")
-  //     {
-  //       TString var_El = "_strip_El_chi2pid";
-  //       TString var_Nuc = "_strip_Nuc_chi2pid";
-
-  //       TString cut_El = auto_cut(var_El, hs_base.at(var_El), cut_label).second;
-  //       TString cut_Nuc = auto_cut(var_Nuc, hs_base.at(var_Nuc), cut_label).second;
-
-  //       cut = cut_El + " && " + cut_Nuc;
-
-  //       // std::cout << "Auto cut" << cut_label << " : " << cut << std::endl;
-  //     }
-  //     else
-  //     {
-  //       TString var;
-  //       if (cut_label == "_mm2_eNg_neutron_expected")
-  //         var = "_mm2_eNg";
-  //       else if (cut_label == "_mm2_eNg_N_nothing_expected")
-  //         var = "_mm2_eNg_N";
-  //       else if (cut_label == "_mm2_eNX_N_photon_expected")
-  //         var = "_mm2_eNX_N";
-  //       else if (cut_label == "_mm2_eg_proton_expected")
-  //         var = "_mm2_eg";
-  //       else if (cut_label == "_delta_t")
-  //         var = "_delta_t";
-  //       else
-  //         continue;
-
-  //       cut = auto_cut(var, hs_base.at(var), cut_label).second;
-
-  //       // std::cout << "Auto cut" << cut_label << " : " << cut << std::endl;
-  //     }
-  //   }
-  // }
 
   std::vector<std::pair<TString, TCut>> cuts;
 
@@ -137,56 +59,31 @@ void stats_legend(TH1D *htemp, TH1D *htemp_cut, const TString &branch_name, cons
 
   gPad->cd();
 
+  htemp->SetMinimum(10.0);
   htemp->Draw("HIST");
   htemp_cut->Draw("HIST SAMES");
   gPad->Update();
 
-  htemp->GetXaxis()->SetTitle(Form("DVCS%s", latex_labels.at(branch_name).Data()));
+  htemp->GetXaxis()->SetTitle(Form("DVCS %s", latex_labels.at(branch_name).Data()));
   htemp->GetYaxis()->SetTitle("Events");
-  htemp->SetMinimum(10.0);
 
   TPaveStats *stats1 = (TPaveStats *)htemp->FindObject("stats");
   TPaveStats *stats2 = (TPaveStats *)htemp_cut->FindObject("stats");
 
-  bool move_stats = should_move_stats(branch_name);
+  stats1->SetX1NDC(0.15);
+  stats1->SetX2NDC(0.33);
+  stats1->SetY1NDC(0.78);
+  stats1->SetY2NDC(0.88);
 
-  if (move_stats)
-  {
-    stats1->SetX1NDC(0.15);
-    stats1->SetX2NDC(0.33);
-    stats1->SetY1NDC(0.78);
-    stats1->SetY2NDC(0.88);
+  stats2->SetX1NDC(0.15);
+  stats2->SetX2NDC(0.33);
+  stats2->SetY1NDC(0.66);
+  stats2->SetY2NDC(0.76);
 
-    stats2->SetX1NDC(0.15);
-    stats2->SetX2NDC(0.33);
-    stats2->SetY1NDC(0.66);
-    stats2->SetY2NDC(0.76);
-  }
-  else
-  {
-    stats1->SetX1NDC(0.80);
-    stats1->SetX2NDC(0.98);
-    stats1->SetY1NDC(0.85);
-    stats1->SetY2NDC(0.95);
-
-    stats2->SetX1NDC(0.80);
-    stats2->SetX2NDC(0.98);
-    stats2->SetY1NDC(0.7);
-    stats2->SetY2NDC(0.8);
-  }
   stats1->SetTextColor(kBlack);
   stats2->SetTextColor(kRed);
 
-  TLegend *legend;
-  if (move_stats)
-  {
-    // legend = new TLegend(0.15, 0.45, 0.33, 0.55);
-    legend = new TLegend(0.36, 0.78, 0.59, 0.88);
-  }
-  else
-  {
-    legend = new TLegend(0.8, 0.55, 0.98, 0.65);
-  }
+  TLegend *legend = new TLegend(0.36, 0.78, 0.54, 0.88);
 
   legend->AddEntry(htemp, "No cuts", "l");
   legend->AddEntry(htemp_cut, "Cuts", "f");
@@ -196,9 +93,6 @@ void stats_legend(TH1D *htemp, TH1D *htemp_cut, const TString &branch_name, cons
 void analysis_data()
 {
 
-  double P_mass = 0.938272;
-  double N_mass = 0.9395654;
-
   TFile *file = TFile::Open("./data/0pDVCS_inbending_FTPhotonsCorrected_test.root");
   TFile *output_file = new TFile("./output_root_hists/analysis_data.root", "RECREATE");
 
@@ -207,19 +101,19 @@ void analysis_data()
   //   tree->Print();
 
   std::vector<std::pair<TString, std::pair<double, double>>> branch_names = {
-      {"_mm2_eg", {-3, 6}},
-      {"_mm2_eNg", {-2, 6}},
-      {"_mm2_eNg_N", {-2, 2}},
-      {"_mm2_eNX_N", {-10, 10}},
-      {"_strip_Q2", {0, 12}},
-      {"_strip_Xbj", {0, 0.8}},
-      {"_t_Nuc", {-12, 1}},
-      {"_t_Ph", {-1, 1}},
+      {"_mm2_eg", {-2, 5.5}},
+      {"_mm2_eNg", {-1.5, 5}},
+      {"_mm2_eNg_N", {-1, 1}},
+      {"_mm2_eNX_N", {-5, 9}},
+      {"_strip_Q2", {1, 8}},
+      {"_strip_Xbj", {0, 0.7}},
+      {"_t_Nuc", {-14, 1}},
+      {"_t_Ph", {-12, 1}},
       {"_delta_t", {-2, 2}},
       {"_Phi_Nuc", {0, 360}},
       {"_Phi_Ph", {0, 360}},
-      {"_delta_Phi", {-3, 3}},
-      {"_strip_El_chi2pid", {-5, 5}},
+      {"_delta_Phi", {-4, 3}},
+      {"_strip_El_chi2pid", {-5.5, 5.5}},
       {"_strip_Ph_chi2pid", {-0.2, 10100}},
       {"_strip_Nuc_chi2pid", {-6, 6}}};
 
@@ -229,21 +123,21 @@ void analysis_data()
   // }
 
   std::map<TString, TString> latex_labels = {
-    {"_mm2_eg", "MM^{2}_{P} e P#rightarrow e'#gamma(P_{miss}) (GeV^2)"},
-    {"_mm2_eNg", "MM^{2}_{P} e D#rightarrow e'P'#gamma(N_{miss}) (GeV^2)"},
-    {"_mm2_eNg_N", "MM^{2}_{X} e P#rightarrow e'P'#gamma (GeV^2)"},
-    {"_mm2_eNX_N", "MM^{2}_{#gamma} e P#rightarrow e'P'(#gamma_{miss}) (GeV^2)"},
-    {"_strip_Q2", "Q^{2}"},
-    {"_strip_Xbj", "x_{B}"},
-    {"_t_Nuc", "t_{Nuc}"},
-    {"_t_Ph", "t_{Ph}"},
-    {"_delta_t", "#Delta t"},
-    {"_Phi_Nuc", "#Phi_{Nuc}"},
-    {"_Phi_Ph", "#Phi_{Ph}"},
-    {"_delta_Phi", "#Delta#Phi"},
-    {"_strip_El_chi2pid", "#chi^{2}_{pid}^{e}"},
-    {"_strip_Ph_chi2pid", "#chi^{2}_{pid}^{#gamma}"},
-    {"_strip_Nuc_chi2pid", "#chi^{2}_{pid}^{N}"}};
+      {"_mm2_eg", "MM^{2}_{P} e P#rightarrow e'#gamma(P_{miss}) (GeV^{2})"},
+      {"_mm2_eNg", "MM^{2}_{P} e D#rightarrow e'P'#gamma(N_{miss}) (GeV^{2})"},
+      {"_mm2_eNg_N", "MM^{2}_{X} e P#rightarrow e'P'#gamma (GeV^{2})"},
+      {"_mm2_eNX_N", "MM^{2}_{#gamma} e P#rightarrow e'P'(#gamma_{miss}) (GeV^{2})"},
+      {"_strip_Q2", "Q^{2}"},
+      {"_strip_Xbj", "x_{B}"},
+      {"_t_Nuc", "t_{Nuc}"},
+      {"_t_Ph", "t_{Ph}"},
+      {"_delta_t", "#Delta t"},
+      {"_Phi_Nuc", "#Phi_{Nuc}"},
+      {"_Phi_Ph", "#Phi_{Ph}"},
+      {"_delta_Phi", "#Delta#Phi"},
+      {"_strip_El_chi2pid", "#chi^{2}_{pid}^{e}"},
+      {"_strip_Ph_chi2pid", "#chi^{2}_{pid}^{#gamma}"},
+      {"_strip_Nuc_chi2pid", "#chi^{2}_{pid}^{N}"}};
 
   std::map<TString, TH1D *> hs_base_data;
 
@@ -256,7 +150,7 @@ void analysis_data()
 
     tree->Project(base_hist_name_data, var, "");
 
-    h_base_data->SetMaximum(1.4 * h_base_data->GetMaximum());
+    h_base_data->SetMaximum(1.5 * h_base_data->GetMaximum());
 
     h_base_data->SetLineColor(kBlack);
     h_base_data->SetStats(true);
@@ -274,18 +168,40 @@ void analysis_data()
   gStyle->SetPadGridX(true);
   gStyle->SetPadGridY(true);
 
+  int plots_per_canvas = 4;
+
   for (const auto &[label_cut, cut] : cuts)
   {
 
-    TCanvas *canvas = new TCanvas("canvas", label_cut, 1920, 1080);
-    canvas->Divide(4, 4);
+    // TCanvas *canvas = new TCanvas("canvas", label_cut, 1920, 1080);
+    // canvas->Divide(4, 4);
+
+    int canvas_index = 0;
+    TCanvas *canvas = nullptr;
 
     for (size_t i = 0; i < branch_names.size(); ++i)
     {
+      if (i % plots_per_canvas == 0)
+      {
+        if (i > 0)
+        {
+          canvas->SaveAs(Form("./cuts_data/optimization_%s_data_%d.png", label_cut.Data(), canvas_index));
+          canvas->SaveAs(Form("./cuts_data/optimization_%s_data_%d.pdf", label_cut.Data(), canvas_index));
+          delete canvas;
+          ++canvas_index;
+        }
+
+        canvas = new TCanvas(Form("canvas_%s_%d", label_cut.Data(), canvas_index),
+                             Form("%s - Part %d", label_cut.Data(), canvas_index),
+                             1600, 1200);
+        canvas->Divide(2, 2);
+      }
+
       const auto &[var, range] = branch_names[i];
       const auto &[min, max] = range;
 
-      canvas->cd(i + 1);
+      canvas->cd(i % plots_per_canvas + 1);
+      // canvas->cd(i + 1);
       if (should_set_logy(var.Data()))
         gPad->SetLogy();
 
@@ -315,12 +231,19 @@ void analysis_data()
       h_cut_data->Write();
     }
 
-    // TString filename = Form("./cuts_no_chi2pid/optimization_%s_no_chi2pid.png", label_cut.Data());
-    TString filename = Form("./cuts_data/optimization_%s_data.png", label_cut.Data());
-    TString filename_pdf = Form("./cuts_data/optimization_%s_data.pdf", label_cut.Data());
-    canvas->SaveAs(filename);
-    canvas->SaveAs(filename_pdf);
-    delete canvas;
+    // // TString filename = Form("./cuts_no_chi2pid/optimization_%s_no_chi2pid.png", label_cut.Data());
+    // TString filename = Form("./cuts_data/AllTogether/optimization_%s_data.png", label_cut.Data());
+    // TString filename_pdf = Form("./cuts_data/AllTogether/optimization_%s_data.pdf", label_cut.Data());
+    // canvas->SaveAs(filename);
+    // canvas->SaveAs(filename_pdf);
+    // delete canvas;
+
+    if (canvas)
+        {
+            canvas->SaveAs(Form("./cuts_data/optimization_%s_data_%d.png", label_cut.Data(), canvas_index));
+            canvas->SaveAs(Form("./cuts_data/optimization_%s_data_%d.png", label_cut.Data(), canvas_index));
+            delete canvas;
+        }
   }
 
   file->Close();
